8935a543275707a9f173a700b6e649b5f1869815
Aria <kehuanwang123@gmail.com>
2025-07-26 12:04:52 +0930

    feat: add image logo to header and configure remote image patterns

--- my-next-app/next.config.ts
+++ my-next-app/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns:[new URL("https://images.unsplash.com/photo-1753204392401-a424cb95d3e2?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]
+  }
 };
 
 export default nextConfig;
--- my-next-app/src/components/header.tsx
+++ my-next-app/src/components/header.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
     return(
         <div className="border-black/10 border-b h-[40px] flex 
         item-center justify-between px-5">
-            <div>Logo</div>
+            <Link href="/">
+                <Image
+                    src="https://images.unsplash.com/photo-1753204392401-a424cb95d3e2?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="logo"
+                    width={30}
+                    height={30}
+                    className="rounded-full"
+                    priority
+                />
+            </Link>
             <ul className="flex items-center gap-3">
                 <li>
                     <Link href="/">Home</Link>
