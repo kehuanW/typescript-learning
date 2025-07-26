8a626e1108e31b8b3437d5fc423d8e0aade70b19
Aria <kehuanwang123@gmail.com>
2025-07-26 12:23:18 +0930

    feat: fetch and display posts from external API in PostsPage component

--- my-next-app/src/app/posts/page.tsx
+++ my-next-app/src/app/posts/page.tsx
@@ -1,8 +1,27 @@
-export default function PostsPage() {
+export default async function PostsPage() {
+  const response = await fetch("https://dummyjson.com/posts");
+  const data = await response.json();
+
+  console.log(data)
+
   return (
     <div className="text-center pt-12">
       <h1 className="text-3xl capitalize font-bold mb-4">Posts</h1>
-      <p className="text-[16px]">List of blog posts will go here.</p>
+      
+      <ul>
+        {
+          data.posts.map((item:{id:number; title:string})=>{
+            return <li key={item.id} className="mb-2">
+              <a href={`/posts/${item.id}`}
+                className="text-blue-500 hover:underline"
+              >
+                {item.title}
+              </a>
+            </li>
+          })
+        }
+      </ul>
+
     </div>
   );
 }
